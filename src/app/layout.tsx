4e7d2fc8cb1bd87a7ac3aeeeaf60f82c7ffde22c
MINTYA <akwaba@akwaba.cm>
2026-04-27 22:51:55 +0100

    feat: agryva  project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,9 @@ export const metadata: Metadata = {
   authors: [{ name: "Agryva Team" }],
   icons: {
     icon: [
-      { url: "/logo.svg", type: "image/svg+xml" },
+      { url: "/icon.svg", type: "image/svg+xml" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
     apple: "/apple-icon.png",
   },
